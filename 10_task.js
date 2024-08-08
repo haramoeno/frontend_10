1b96b73f30595256ded541df57c637da2d706a34
haramoeno <moe.piece1201@ezweb.ne.jp>
2024-08-08 11:13:20 +0000

    課題提出

---
+++ 10_task.js
@@ -0,0 +1,20 @@
+let apple = {
+    color: 'red',
+    size: 10,
+    weight: 200
+};
+
+let mandarin = {
+    color: 'orange',
+    size: 6,
+    weight: 100
+};
+
+let grape = {
+    color: 'purple',
+    size: 8,
+    weight: 500
+};
+
+let fruits = ['apple','mandarin','grape'];
+console.log(fruits);
