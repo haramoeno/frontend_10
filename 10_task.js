22fa0e0f4e0f95dbf7e97bfc35524e082aa98353
haramoeno <moe.piece1201@ezweb.ne.jp>
2024-08-22 09:18:25 +0000

    課題提出

--- 10_task.js
+++ 10_task.js
@@ -1,20 +1,37 @@
 let apple = {
     color: 'red',
     size: 10,
-    weight: 200
+    weight: 100
 };
 
 let mandarin = {
     color: 'orange',
     size: 6,
-    weight: 100
+    weight: 200
 };
 
 let grape = {
     color: 'purple',
     size: 8,
-    weight: 500
+    weight: 300
 };
 
-let fruits = ['apple','mandarin','grape'];
+let banana = {
+    color: 'purple',
+    size: 8,
+    weight: 300
+};
+
+let fruits = [apple,mandarin,grape,];
 console.log(fruits);
+
+
+let total_weight = 0;
+for (let i = 0 ; i < fruits.length; i++) {
+    // console.log(fruits[i].weight);
+    total_weight = total_weight + fruits[i].weight;
+    // total_weight += fruits[i].weight;
+    
+}
+console.log(total_weight);
+
